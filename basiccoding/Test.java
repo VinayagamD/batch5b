08b3e2d967dc613c36618e192a168f446277caea
VinayagamD <vinayagam.d.ganesh@gmail.com>
2020-08-30 12:23:14 +0530

    Basic Java Coding

---
+++ basiccoding/Test.java
@@ -0,0 +1,9 @@
+class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello To Java Training");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
